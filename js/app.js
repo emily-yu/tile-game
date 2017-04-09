a3f7fe2eaebffa18ef6815e5cfbbc4b6a68d76ff
ey1031 <eyudeveloper@gmail.com>
2017-04-09 15:48:18 -0700

    remove excess files, add game generation

--- js/app.js
+++ js/app.js
@@ -6,6 +6,76 @@ console.log("Game Loaded!")
 let button = new Array();
 
 // FUNCTIONS
+let userInput = document.getElementById('userInput');
+let createNew = document.getElementById('addButton');
+createNew.addEventListener("click", function() {
+
+	removeElementsByClass('game-button')
+	button = new Array();
+	// CLEAR BUTTON JSON THEN WE OUTTT
+
+    if ((userInput.value%1) != 0 || userInput.value == "") {
+        alert("Please input a number.");
+        return false;
+    }
+    else {
+        createButton(userInput.value)
+
+		// GET THE OBJECTS
+		let documentButtons = document.getElementsByClassName('game-button');
+		let winText = document.getElementById('winText');
+		// let reloadButton = document.getElementById('reloadButton');
+		winText.style.visibility='hidden' 
+		let counter = documentButtons.length;
+		console.log(documentButtons.length)
+
+		 // ARRAY WITH ALL BUTTON INDEXES
+		let numbArray = []
+		for (let i = 0; i < counter; i += 1) {
+			numbArray.push(i + 1)
+		}
+
+		// ADD OBJECTS AND ASSIGN THEIR ASSOCIATIONS
+		for (let i = 0; i < counter; i += 1) {
+			int = roll(documentButtons, numbArray) // number to assign
+			newButton(numbArray[int], false)
+			numbArray.splice(int ,1);
+
+			// changes state of button clicked
+			documentButtons[i].addEventListener("click", function() {
+		    	console.log("you clicked");
+				onClick((this.textContent)-1)
+				console.log(JSON.stringify(button));
+				checkWin(counter)
+			});
+		}
+
+		console.log(JSON.stringify(button)); // CURRENT JSON OBJECT
+
+    }
+    
+})
+
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
+
+function createButton(key) {
+	for (let i = 0; i < key; i += 1) {
+		var button1 = document.createElement('a')
+	    button1.className += "btn btn-primary btn-xl game-button";
+	    button1.setAttribute("style", "border-radius:0%; margin-top:5px; text-align: center; margin-left:5px");
+	    button1.innerHTML = i +1;
+	    document.getElementById("append").appendChild(button1); // this is working
+	    console.log("same")
+	    console.log(button1)
+	}
+}
+
+
 // CHANGES STATE TO OPPOSITE
 function onClick(key){
 	button[key]["state"] = !(button[key]["state"])
@@ -16,7 +86,7 @@ function onClick(key){
 }
 
 // CHANGE CSS BASED ON CLICKED OR NOT    
-function modifyCSS(key) {
+function modifyCSS(key, documentButtons) {
 	let x = button[key]["associationValue"]
 	if (button[x-1]["state"] = true) {
 		documentButtons[key].setAttribute('style','background-color: #f2f2f2;')
@@ -36,7 +106,7 @@ function newButton (associationValue, state) {
 }
 
 // RANDOM ASSOCIATIONS
-function roll() {
+function roll(documentButtons, numbArray) {
 	let rand = documentButtons[Math.floor(Math.random() * numbArray.length)];
 	let int = rand.textContent; // gets number inside
 	return int - 1
@@ -44,7 +114,7 @@ function roll() {
 
 // CHECK IF THE PLAYER HAS WON
 let statesArray = []
-function checkWin() {
+function checkWin(counter) {
 	// get all the states
 	for (let i = 0; i < counter; i += 1) {
 		let states = button[i]['state'];
@@ -53,7 +123,6 @@ function checkWin() {
 	console.log(statesArray)
 
 	if (statesArray.every(didWin)) {
-		console.log("ASJDLKFJKLDSJFKLSDAJF GOTEEM") // PRINTS WHEN YOU WIN WOOOOOOOOOOOO WE DID IT BOYYYYS
 		winText.className += "fadeInText";
 		winText.style = "visible"
 	}
@@ -64,32 +133,3 @@ function checkWin() {
 function didWin(parameter) {
     return parameter == true;
 }
-
-// GET THE OBJECTS
-let documentButtons = document.getElementsByClassName('game-button');
-let winText = document.getElementById('winText');
-winText.style.visibility='hidden' 
-let counter = documentButtons.length;
-
- // ARRAY WITH ALL BUTTON INDEXES
-let numbArray = []
-for (let i = 0; i < counter; i += 1) {
-	numbArray.push(i + 1)
-}
-
-// ADD OBJECTS AND ASSIGN THEIR ASSOCIATIONS
-for (let i = 0; i < counter; i += 1) {
-	int = roll() // number to assign
-	newButton(numbArray[int], false)
-	numbArray.splice(int ,1);
-
-	// changes state of button clicked
-	documentButtons[i].addEventListener("click", function() {
-    	console.log("you clicked");
-		onClick((this.textContent)-1)
-		console.log(JSON.stringify(button));
-		checkWin()
-	});
-}
-
-console.log(JSON.stringify(button)); // CURRENT JSON OBJECT
