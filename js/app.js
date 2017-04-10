952a565bb75d657dc1b80560c32b7eee0e589da0
ey1031 <eyudeveloper@gmail.com>
2017-04-09 18:01:26 -0700

    final

--- js/app.js
+++ js/app.js
@@ -2,100 +2,80 @@
 
 console.log("Game Loaded!")
 
-// JSON TO STORE
 let button = new Array();
+let userInput = document.getElementById('userInput'); // user's number of buttons
+let createNew = document.getElementById('addButton'); // create new game buttons
+createNew.addEventListener("click", function() { // adds the buttons
+	removeElementsByClass('game-button')	// remove any previous game buttons
+	button = new Array();	// clear tracking array
 
-// FUNCTIONS
-let userInput = document.getElementById('userInput');
-let createNew = document.getElementById('addButton');
-createNew.addEventListener("click", function() {
-
-	removeElementsByClass('game-button')
-	button = new Array();
-	// CLEAR BUTTON JSON THEN WE OUTTT
-
+	// checks to make sure that userInput is a number, not a String
     if ((userInput.value%1) != 0 || userInput.value == "") {
-        alert("Please input a number.");
+        alert("Please input a valid number.");
         return false;
     }
     else {
+    	// create buttons from 1 to userInput
         createButton(userInput.value)
 
-		// GET THE OBJECTS
+		// get all the buttons
 		let documentButtons = document.getElementsByClassName('game-button');
 		let winText = document.getElementById('winText');
 		winText.style.visibility='hidden' 
 		let counter = documentButtons.length;
 		console.log(documentButtons.length)
 
-		 // ARRAY WITH ALL BUTTON INDEXES
+		 // stores the button indexes
 		let numbArray = []
 		for (let i = 0; i < counter; i += 1) {
 			numbArray.push(i + 1)
 		}
 
-		// ADD OBJECTS AND ASSIGN THEIR ASSOCIATIONS
+		// add objects to the tracking system
 		for (let i = 0; i < counter; i += 1) {
 			int = roll(documentButtons, numbArray) // number to assign
 			newButton(numbArray[int], false)
-			numbArray.splice(int ,1);
+			numbArray.splice(int,1);
 
 			// changes state of button clicked
 			documentButtons[i].addEventListener("click", function() {
-		    	console.log("you clicked");
 				onClick((this.textContent)-1)
 				console.log(JSON.stringify(button));
 				checkWin(counter)
 			});
 		}
-
-		console.log(JSON.stringify(button)); // CURRENT JSON OBJECT
-
+		console.log(JSON.stringify(button)); // prints to console the true-false status
     }
-    
 })
 
+// remove elements with class className
 function removeElementsByClass(className){
-    var elements = document.getElementsByClassName(className);
+    let elements = document.getElementsByClassName(className);
     while(elements.length > 0){
         elements[0].parentNode.removeChild(elements[0]);
     }
 }
 
+// create buttons from 1 to key
 function createButton(key) {
 	for (let i = 0; i < key; i += 1) {
-		var button1 = document.createElement('a')
+		let button1 = document.createElement('a')
 	    button1.className += "btn btn-primary btn-xl game-button";
 	    button1.setAttribute("style", "border-radius:0%; margin-top:5px; text-align: center; margin-left:5px");
 	    button1.innerHTML = i +1;
-	    document.getElementById("append").appendChild(button1); // this is working
-	    console.log("same")
-	    console.log(button1)
+	    document.getElementById("append").appendChild(button1);
 	}
 }
 
-
-// CHANGES STATE TO OPPOSITE
+// change state of button 'key' to the opposite of what it is
 function onClick(key){
 	button[key]["state"] = !(button[key]["state"])
 	let x = button[key]["associationValue"]
 	console.log(x) 
 	button[x-1]["state"] = !(button[x-1]["state"])
-	// modifyCSS(key)
 }
 
-// CHANGE CSS BASED ON CLICKED OR NOT    
-function modifyCSS(key, documentButtons) {
-	let x = button[key]["associationValue"]
-	if (button[x-1]["state"] = true) {
-		documentButtons[key].setAttribute('style','background-color: #f2f2f2;')
-	}
-	else {
-		documentButtons[key].setAttribute('style','background-color: #49A154;');
-	}
-}
-
-// NEW BUTTON
+// create a new button in tracking system
 function newButton (associationValue, state) {
 	let toInsert = {
 		"associationValue": associationValue,
@@ -104,14 +84,14 @@ function newButton (associationValue, state) {
 	button.push(toInsert);  
 }
 
-// RANDOM ASSOCIATIONS
+// create random associations
 function roll(documentButtons, numbArray) {
 	let rand = documentButtons[Math.floor(Math.random() * numbArray.length)];
 	let int = rand.textContent; // gets number inside
 	return int - 1
 }
 
-// CHECK IF THE PLAYER HAS WON
+// check to see if the player has won
 let statesArray = []
 function checkWin(counter) {
 	// get all the states
@@ -128,7 +108,6 @@ function checkWin(counter) {
 	statesArray.length = 0
 	console.log(statesArray)
 }
-
 function didWin(parameter) {
     return parameter == true;
 }
